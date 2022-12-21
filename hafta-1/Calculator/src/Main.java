3f093fa9af4e9d9b7d2fe0a67e63d310bf88bbce
Hasan Polat <thatsnotpublic@gmail.com>
2022-12-21 14:56:56 +0300

    Add files via upload

---
+++ hafta-1/Calculator/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int wallet = 5000, operation, amount;
+
+        while(true) {
+
+            System.out.println("1. Bakiye öğrenmek için tuşlayınız");
+            System.out.println("2. Para yatırmak için tuşlayınız");
+            System.out.println("3. Para çekmek için tuşlayınız");
+            System.out.println("4. Hesaptan çıkmak için tuşlayınız");
+
+            System.out.print("Yapmak istediğiniz işlemin numarasını giriniz: ");
+            operation = s.nextInt();
+
+            switch (operation) {
+                case 1:
+                    System.out.println("Bakiye " + wallet + " TL");
+                    break;
+
+                case 2:
+                    System.out.print("Yatırmak istediğiniz miktarı giriniz: ");
+                    amount = s.nextInt();
+                    wallet += amount;
+
+                    System.out.println("Bakiye " + wallet + " TL");
+                    break;
+
+                case 3:
+                    System.out.print("Çekmek istediğiniz miktarı giriniz: ");
+                    amount = s.nextInt();
+                    if (amount > wallet) {
+                        System.out.println("Yeterli bakiyeniz yoktur.");
+                    } else {
+                        wallet -= amount;
+                        System.out.println("Bakiye " + wallet + " TL");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Hesaptan çıkış yapıldı. İyi günler.");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Geçersiz işlem!");
+            }
+        }
+    }
+}
---
+++ hafta-1/FindNumber/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int total = 0;
+
+        while (true) {
+            int number = random.nextInt(500);
+            if (number > 49 && number < 101) {
+                total++;
+                System.out.print(number + " sayısı " + total + ". seferde bulundu.");
+                break;
+            }
+            total++;
+        }
+    }
+}
