a883f5ac6c7d785dafc672ef4413ddb3c4d6358d
Hasan Polat <thatsnotpublic@gmail.com>
2023-01-19 15:37:53 +0300

    classes created (bakalım)

---
+++ Fruits/src/main/java/com/example/fruits/Fruit.java
@@ -0,0 +1,47 @@
+package com.example.fruits;
+
+public class Fruit {
+    private Integer number;
+    private String name;
+    private Double price;
+    private Integer stock;
+
+    public Fruit(Integer number, String name, Double price, Integer stock) {
+        this.number = number;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public java.lang.Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(java.lang.Integer number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
